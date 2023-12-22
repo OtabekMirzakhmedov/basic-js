1289a480bdcc6a1ba0d05cd5a080c64dd81ca19e
User <User@DESKTOP-C51DDIB>
2023-12-22 11:43:39 +0500

    delete digit, dns stats, dream team

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new Error('Input must be a valid number');
+  }
+
+  const numStr = String(n);
+  let maxNumber = 0;
+
+  for (let i = 0; i < numStr.length; i++) {
+    const modifiedNumber = parseInt(numStr.slice(0, i) + numStr.slice(i + 1));
+    if (modifiedNumber > maxNumber) {
+      maxNumber = modifiedNumber;
+    }
+  }
+
+  return maxNumber;
 }
 
 module.exports = {
